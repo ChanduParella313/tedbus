10d1e3c37e58fd448a637c727a215e59c9e19dd1
ChanduParella313 <111419267+ChanduParella313@users.noreply.github.com>
2025-01-21 16:15:09 +0530

    Update index.js

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -14,7 +14,7 @@ app.use(bookingroute)
 app.use(routesroute)
 app.use(customerroutes)
 
-const DBURL="mongodb+srv://admin:admin@tedbus.vqk1yid.mongodb.net/?retryWrites=true&w=majority&appName=tedbus"
+const DBURL="mongodb+srv://tudbus:tudbus@cluster0.3qjio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 mongoose.connect(DBURL)
 .then(()=> console.log("Mongodb connected"))
 .catch(err=> console.error('Mongodb connection error:' ,err))
